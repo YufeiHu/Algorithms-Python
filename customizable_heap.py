36ec6346b3469018abad6334fd7d6c1e6872405d
Yufei Hu <yufeihu@g.ucla.edu>
2019-01-21 22:36:40 -0800

    Add files via upload

---
+++ customizable_heap.py
@@ -0,0 +1,60 @@
+import heapq
+
+
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class customizable_heap(object):
+
+    def __init__(self, data=None, comparator=lambda x: x):
+        self.comparator = comparator
+        self.cnt = 0
+        if data:
+            self._data = list()
+            for val in data:
+                self._data.append((self.comparator(val), self.cnt, val))
+                self.cnt += 1
+            heapq.heapify(self._data)
+        else:
+            self._data = list()
+
+    def push(self, item):
+        heapq.heappush(self._data, (self.comparator(item), self.cnt, item))
+        self.cnt += 1
+
+    def pop(self):
+        if self._data:
+            self.cnt -= 1
+            return heapq.heappop(self._data)[2]
+        else:
+            return None
+
+    def top(self):
+        if self._data:
+            return self._data[0][2]
+        else:
+            return None
+
+
+def comparator(point):
+    return point.x ** 2 + point.y ** 2
+
+
+if __name__ == "__main__":
+    customizable_heap_inst = customizable_heap(comparator=comparator)
+
+    p1 = point(1, 2)
+    p2 = point(2, 1)
+    p3 = point(2, 3)
+    p4 = point(3, 4)
+    myHeapInst.push(p4)
+    myHeapInst.push(p2)
+    myHeapInst.push(p1)
+    myHeapInst.push(p3)
+    myHeapInst.pop()
+
+    print(myHeapInst.top().x, myHeapInst.top().y)
+    
---
+++ kruskal.py
@@ -0,0 +1,100 @@
+from collections import defaultdict
+
+
+class union_find(object):
+
+    def __init__(self, num_elements):
+        self.parents = list(range(num_elements))
+        self.size_component = [1] * num_elements
+        self.num_elements = num_elements
+        self.num_components = num_elements
+
+    def find(self, idx):
+        # find its root first
+        root = idx
+        while root != self.parents[root]:
+            root = self.parents[root]
+
+        # path compression
+        while idx != root:
+            idx_next = self.parents[idx]
+            self.parents[idx] = root
+            idx = idx_next
+
+        # return the root
+        return root
+
+    def union(self, i1, i2):
+        root1 = self.find(i1)
+        root2 = self.find(i2)
+        if root1 == root2: return
+
+        if self.size_component[root1] < self.size_component[root2]:
+            self.size_component[root2] += self.size_component[root1]
+            self.parents[root1] = root2
+        else:
+            self.size_component[root1] += self.size_component[root2]
+            self.parents[root2] = root1
+
+        self.num_components -= 1
+
+    def print_clusters(self):
+        clusters = defaultdict(list)
+        for i in range(self.num_elements):
+            root = self.find(i)
+            clusters[root].append(i)
+
+        for root, children in clusters.items():
+            print(children)
+
+        print("Number of components = {}".format(self.num_components))
+
+
+"""
+1. Sort all the edges in non-decreasing order of their weight.
+2. Pick the smallest edge. Check if it forms a cycle with the spanning tree formed so far. 
+   If cycle is not formed, include this edge. Else, discard it.
+3. Repeat step#2 until there are (V-1) edges in the spanning tree.
+Only undirected graph is supported!
+"""
+def kruskal(edges):
+    # get the number of vertices
+    vertices = set()
+    for v1, v2, weight in edges:
+        vertices.add(v1)
+        vertices.add(v2)
+    num_vertices = len(vertices)
+
+    # step 1: sort all edges in non-decreasing order of their weight
+    edges = sorted(edges, key=lambda x : x[2])
+    ans = list()
+    union_find_inst = union_find(num_vertices)
+
+    # step 2: pick the smallest-weight edge each time
+    for edge in edges:
+        if len(ans) >= num_vertices - 1:
+            break
+
+        v1, v2, weight = edge
+        parent_v1 = union_find_inst.find(v1)
+        parent_v2 = union_find_inst.find(v2)
+
+        # when no cycle is found, pick the current edge
+        if parent_v1 != parent_v2:
+            ans.append([v1, v2, weight])
+            union_find_inst.union(parent_v1, parent_v2)
+
+    return ans
+
+
+if __name__ == "__main__":
+    edges = [(0, 1, 10),
+             (0, 2, 6),
+             (0, 3, 5),
+             (1, 3, 15),
+             (2, 3, 4)]
+
+    ans = kruskal(edges)
+
+    for v1, v2, weight in ans:
+        print("{} -> {} with cost of {}".format(v1, v2, weight))
