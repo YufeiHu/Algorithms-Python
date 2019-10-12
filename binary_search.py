c3070c528299dda7672215f9f4564d290c503b95
Yufei Hu <yufeihu@g.ucla.edu>
2019-10-12 16:34:36 -0700

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,12 @@
 def binary_search(nums, target):
     l = 0
     r = len(nums) - 1
-    while l < r:
+    while l <= r:
         m = (l + r) // 2
         if nums[m] < target:
             l = m + 1
         elif nums[m] > target:
-            r = m
+            r = m - 1
         else:
             return m
     return -1
